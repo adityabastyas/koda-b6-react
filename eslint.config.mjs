e73cdfd9076b40ef186ac56d716e031d860964aa
adityabastyas <adityabastyasmulya@gmail.com>
2026-01-22 09:51:04 +0700

    chore: setup eslint and install moment

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js, react: pluginReact },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.node },
+  },
+  pluginReact.configs.flat.recommended,
+]);
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  rules: {
+    semi: ["error", "always"],
+    indent: ["error", 2],
+    quotes: ["error", "double"],
+    "no-var": "error",
+    "no-unused-vars": ["warn"],
+    eqeqeq: "warn",
+    curly: ["error", "all"],
+  },
 });
