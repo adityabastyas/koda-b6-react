ebce92526f2a2120cfe49514f33de4c7ffd0a3b3
adityabastyas <adityabastyasmulya@gmail.com>
2026-02-10 00:41:53 +0700

    feat: add modal to register page and add settimeout to navigate signin

--- src/components/ModalLoading.jsx
+++ src/components/ModalLoading.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
-function ModalLoading() {
-  return <div>ModalLoading</div>;
+function ModalLoading(props) {
+  const { isOke, text = "" } = props;
+
+  if (!isOke) {
+    return;
+  }
+  return (
+    <section className='fixed inset-0 z-5 flex items-center justify-center bg-black/50'>
+      <div className='bg-white px-6 py-10 rounded-2xl shadow-lg flex flex-col items-center gap-3'>
+        <p className='text-2xl text-gray-700'>{text}</p>
+      </div>
+    </section>
+  );
 }
 
 export default ModalLoading;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Input from "../components/Input";
 import backgroundCoffe from "../assets/img/Rectangle 289.png";
 import Button from "../components/Button";
@@ -7,6 +8,7 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { useDispatch } from "react-redux";
 import { registerUser } from "../redux/reduces/authReducer";
+import ModalLoading from "../components/ModalLoading";
 
 const validation = yup.object({
   fullName: yup.string().required("full name wajib diisi"),
@@ -30,6 +32,7 @@ const validation = yup.object({
 });
 
 function Register() {
+  const [show, setShow] = React.useState(false);
   const { register, handleSubmit, formState } = useForm({
     defaultValues: {
       email: "",
@@ -47,142 +50,151 @@ function Register() {
   const formSubmit = (nilai) => {
     dispatch(registerUser(nilai));
 
-    navigate("/signin");
+    setShow(true);
+
+    setTimeout(() => {
+      setShow(false);
+
+      navigate("/signin");
+    }, 2000);
   };
   return (
-    <div className='flex'>
-      <div className='hidden sm:block w-[40%] '>
-        <img
-          src={backgroundCoffe}
-          alt='cooffe bg image'
-          className='object-cover w-full h-full '
-        />
-      </div>
-
-      <main className='font-[Plus_Jakarta_Sans,sans-serif] flex-1 py-12 px-5 sm:pr-32 sm:pl-16'>
-        <header>
-          <div className='flex items-center gap-2 mb-8'>
-            <img src='./src/assets/img/icon/Frame.svg' alt='icon coffie' />
-            <span className='font-[Sacramento,cursive]'>Coffee Shop</span>
-          </div>
-          <h1 className='text-[#8E6447] font-semibold text-2xl'>Register</h1>
-          <p className='my-4'>Fill out the form correctly</p>
-        </header>
-        <form
-          onSubmit={handleSubmit(formSubmit)}
-          className='flex flex-col gap-6'
-        >
-          <div>
-            <Input
-              htmlFor='fullName'
-              label='Full Name'
-              id='fullName'
-              placeholder='Enter Your Full Name'
-              src='./src/assets/img/icon/Profile.svg'
-              alt='icon profile'
-              {...register("fullName")}
-            />
-            {formState.errors.fullName && (
-              <p className='text-red-700'>
-                {formState.errors.fullName.message}
-              </p>
-            )}
-          </div>
-
-          <div>
-            <Input
-              label='Email'
-              htmlFor='email'
-              id='email'
-              placeholder='Enter Your Email'
-              src='src\assets\img\icon\mail.svg'
-              alt='icon email'
-              {...register("email")}
-            />
-            {formState.errors.email && (
-              <p className='text-red-700'>{formState.errors.email.message}</p>
-            )}
-          </div>
-
-          <div>
-            <Input
-              label='Password'
-              htmlFor='password'
-              id='password'
-              placeholder='Enter Your Password'
-              src='src\assets\img\icon\Password.svg'
-              alt='icon password'
-              type='password'
-              {...register("password")}
-            />
-            {formState.errors.password && (
-              <p className='text-red-700'>
-                {formState.errors.password.message}
-              </p>
-            )}
-          </div>
-
-          <div>
-            <Input
-              label='Confirm Password'
-              htmlFor='confirmPassword'
-              id='confirmPassword'
-              placeholder='Enter Your Password Again'
-              src='src\assets\img\icon\Password.svg'
-              alt='icon password'
-              type='password'
-              {...register("confirmPassword")}
-            />
-            {formState.errors.confirmPassword && (
-              <p className='text-red-700'>
-                {formState.errors.confirmPassword.message}
-              </p>
-            )}
-          </div>
-
-          <Button type='submit'>Register</Button>
-        </form>
-
-        <footer className='text-center my-6 text-base text-[#4f5665]'>
-          <p>
-            Have An Account?
-            <span className='decoration-none text-[#ff8906]'>
-              <Link to='/signin'>Login</Link>
-            </span>
-          </p>
-        </footer>
-
-        <div className='text-center flex justify-center gap-12 items-center'>
-          <div className='border-b border-[#aaaaaa] flex-2'></div>
-          <span className='text-[#aaaaaa]'>Or</span>
-          <div className='border-b border-[#aaaaaa] flex-2'></div>
+    <>
+      <ModalLoading isOke={show} text='Register berhasil' />
+      <div className='flex'>
+        <div className='hidden sm:block w-[40%] '>
+          <img
+            src={backgroundCoffe}
+            alt='cooffe bg image'
+            className='object-cover w-full h-full '
+          />
         </div>
 
-        <div className='mt-6 text-center flex justify-center items-center gap-8 '>
-          <a
-            href='https://www.facebook.com/'
-            target='_blank'
-            className='flex items-center gap-2 bg-white rounded-2xl px-6 py-4 text-[#4f5665] font-medium text-lg shadow-[0_4px_10px_rgba(0,0,0,0.08)] md:px-28 md:py-5'
+        <main className='font-[Plus_Jakarta_Sans,sans-serif] flex-1 py-12 px-5 sm:pr-32 sm:pl-16'>
+          <header>
+            <div className='flex items-center gap-2 mb-8'>
+              <img src='./src/assets/img/icon/Frame.svg' alt='icon coffie' />
+              <span className='font-[Sacramento,cursive]'>Coffee Shop</span>
+            </div>
+            <h1 className='text-[#8E6447] font-semibold text-2xl'>Register</h1>
+            <p className='my-4'>Fill out the form correctly</p>
+          </header>
+          <form
+            onSubmit={handleSubmit(formSubmit)}
+            className='flex flex-col gap-6'
           >
-            <img
-              src='
+            <div>
+              <Input
+                htmlFor='fullName'
+                label='Full Name'
+                id='fullName'
+                placeholder='Enter Your Full Name'
+                src='./src/assets/img/icon/Profile.svg'
+                alt='icon profile'
+                {...register("fullName")}
+              />
+              {formState.errors.fullName && (
+                <p className='text-red-700'>
+                  {formState.errors.fullName.message}
+                </p>
+              )}
+            </div>
+
+            <div>
+              <Input
+                label='Email'
+                htmlFor='email'
+                id='email'
+                placeholder='Enter Your Email'
+                src='src\assets\img\icon\mail.svg'
+                alt='icon email'
+                {...register("email")}
+              />
+              {formState.errors.email && (
+                <p className='text-red-700'>{formState.errors.email.message}</p>
+              )}
+            </div>
+
+            <div>
+              <Input
+                label='Password'
+                htmlFor='password'
+                id='password'
+                placeholder='Enter Your Password'
+                src='src\assets\img\icon\Password.svg'
+                alt='icon password'
+                type='password'
+                {...register("password")}
+              />
+              {formState.errors.password && (
+                <p className='text-red-700'>
+                  {formState.errors.password.message}
+                </p>
+              )}
+            </div>
+
+            <div>
+              <Input
+                label='Confirm Password'
+                htmlFor='confirmPassword'
+                id='confirmPassword'
+                placeholder='Enter Your Password Again'
+                src='src\assets\img\icon\Password.svg'
+                alt='icon password'
+                type='password'
+                {...register("confirmPassword")}
+              />
+              {formState.errors.confirmPassword && (
+                <p className='text-red-700'>
+                  {formState.errors.confirmPassword.message}
+                </p>
+              )}
+            </div>
+
+            <Button type='submit'>Register</Button>
+          </form>
+
+          <footer className='text-center my-6 text-base text-[#4f5665]'>
+            <p>
+              Have An Account?
+              <span className='decoration-none text-[#ff8906]'>
+                <Link to='/signin'>Login</Link>
+              </span>
+            </p>
+          </footer>
+
+          <div className='text-center flex justify-center gap-12 items-center'>
+            <div className='border-b border-[#aaaaaa] flex-2'></div>
+            <span className='text-[#aaaaaa]'>Or</span>
+            <div className='border-b border-[#aaaaaa] flex-2'></div>
+          </div>
+
+          <div className='mt-6 text-center flex justify-center items-center gap-8 '>
+            <a
+              href='https://www.facebook.com/'
+              target='_blank'
+              className='flex items-center gap-2 bg-white rounded-2xl px-6 py-4 text-[#4f5665] font-medium text-lg shadow-[0_4px_10px_rgba(0,0,0,0.08)] md:px-28 md:py-5'
+            >
+              <img
+                src='
             src\assets\img\icon\facebook.svg
             '
-              alt='facebook icon'
-            />
-            <span className='hidden sm:inline'>Facebook</span>
-          </a>
-          <a
-            href='https://www.google.com/'
-            target='_blank'
-            className='flex items-center gap-2 bg-white rounded-2xl px-6 py-4 text-[#4f5665] font-medium text-lg shadow-[0_4px_10px_rgba(0,0,0,0.08)]  md:px-28 md:py-5'
-          >
-            <img src='src/assets/img/icon/google.svg' alt='google icon' />
-            <span className='hidden md:inline'>Google</span>
-          </a>
-        </div>
-      </main>
-    </div>
+                alt='facebook icon'
+              />
+              <span className='hidden sm:inline'>Facebook</span>
+            </a>
+            <a
+              href='https://www.google.com/'
+              target='_blank'
+              className='flex items-center gap-2 bg-white rounded-2xl px-6 py-4 text-[#4f5665] font-medium text-lg shadow-[0_4px_10px_rgba(0,0,0,0.08)]  md:px-28 md:py-5'
+            >
+              <img src='src/assets/img/icon/google.svg' alt='google icon' />
+              <span className='hidden md:inline'>Google</span>
+            </a>
+          </div>
+        </main>
+      </div>
+    </>
   );
 }
 
