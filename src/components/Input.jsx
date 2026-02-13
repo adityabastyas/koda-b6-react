de1d24680cf96751d99859490621f319d3401937
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-02-13 17:44:09 +0700

    feat: add icon eye close and eye open in form password

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,5 +1,13 @@
+import React from "react";
+import { FaEye } from "react-icons/fa";
+import { IoMdEyeOff } from "react-icons/io";
+
 function Input(props) {
   const { type, label, id, src, placeholder, alt, ...rest } = props;
+
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const isPassword = type === "password";
   return (
     <div className='flex flex-col gap-1'>
       <label htmlFor={id} className='text-base font-semibold text-[#0B132A]'>
@@ -9,12 +17,22 @@ function Input(props) {
         <img src={src} alt={alt} />
         <input
           id={id}
-          type={type}
+          type={isPassword && showPassword ? "text" : type}
           placeholder={placeholder}
           {...rest}
           className='outline-none w-full text-xs text-[#4F5665]'
         />
+        {isPassword && (
+          <button
+            type="button"
+            onClick={() => setShowPassword(!showPassword)}
+            className="ml-2 text-gray-500"
+          >
+            {showPassword ? <IoMdEyeOff /> : <FaEye />}
+          </button>
+        )}
       </div>
+      
     </div>
   );
 }
