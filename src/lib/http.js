a7a4feddbb73a92cd4457a46d64b728d3324801a
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-14 15:09:36 +0700

    feat: refactor HTTP headers setup to conditionally include Content-Type

--- src/lib/http.js
+++ src/lib/http.js
@@ -9,9 +9,11 @@ const BASE_URL = import.meta.env.VITE_BASE_URL || "https://adit-backend.camps.fa
 async function http(url,body, opts={}){
   const state = store.getState();
   const token = state.auth.token;
-  const headers = {
-    "Content-Type": "application/json",
-  };
+  const headers = {};
+
+  if (!(body instanceof FormData)) {
+    headers["Content-Type"] = "application/json";
+  }
 
   if(token){
     headers.Authorization = "Bearer "+ token;
